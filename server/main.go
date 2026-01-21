f097568f92c412f4133f33514289d531ee31a101
VikramChauhan19 <vikram9696chauhan@gmail.com>
2026-01-22 01:07:53 +0530

    added cors and air

--- server/main.go
+++ server/main.go
@@ -1,41 +1,47 @@
 package main
 
 import (
-	"log"
-	"os"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"github.com/vikramchauhan19/BLOG_GO/database"
 	"github.com/vikramchauhan19/BLOG_GO/router"
-	
+	"log"
+	"os"
 )
 
-func init(){
+func init() {
 	database.ConnectDB() // connection with db
 }
-func main(){
-	if os.Getenv("ENV") != "production"{ // load ker rhe he .env k variable
+func main() {
+	if os.Getenv("ENV") != "production" { // load ker rhe he .env k variable
 		err := godotenv.Load(".env")
-		if err != nil{
+		if err != nil {
 			log.Fatal("error loading .env file")
 		}
 	}
-	sqlDB,err := database.DBConnection.DB() // give advance control
-	if err != nil{
+	sqlDB, err := database.DBConnection.DB() // give advance control
+	if err != nil {
 		panic("Error in sql Connection")
 	}
 	defer sqlDB.Close()
 
-
 	app := fiber.New() //creating server
-	app.Use(logger.New())
+
+	app.Use(cors.New(cors.Config{ //not req for production because FE and BE must serve under same domain
+		AllowHeaders: "Origin, Content-Type,Accept",
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: "GET,POST,PUT,DELETE,PATCH",
+	}))
+	app.Use(logger.New()) // terminal pe req dekhata he
+	
 	router.SetUpRoutes(app)
-	
-	app.Get("/",func(c *fiber.Ctx)error{
-		return c.Status(200).JSON(fiber.Map{"message":"Welcome to my server"})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "Welcome to my server"})
 	})
-	
+
 	PORT := os.Getenv("PORT")
-	app.Listen("0.0.0.0:"+PORT) //0.0.0.0 listen everywhere
+	app.Listen("0.0.0.0:" + PORT) //0.0.0.0 listen everywhere
 }
